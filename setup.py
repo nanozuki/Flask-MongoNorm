3a7ad6c7772cf55b86c1a70650a294bcc7fd173c
crows <pt.wenhan@gmail.com>
2017-04-12 19:51:55 +0800

    init project

---
+++ setup.py
@@ -0,0 +1,38 @@
+"""
+Flask-MongoNorm
+-------------
+
+MongoNorm support for Flask applications.
+"""
+from setuptools import find_packages, setup
+
+setup(
+    name='Flask-MongoNorm',
+    version='0.0.1',
+    url='https://github.com/CrowsT/Flask-MongoNorm',
+    license='BSD',
+    author='Crows',
+    author_email='pt.wenhan@gmail.com',
+    description='MongoNorm support for Flask applications',
+    long_description=__doc__,
+    platforms='any',
+    packages=['flask_mongonorm'],
+    install_requires=[
+        'MongoNorm(>=2.0)'
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
